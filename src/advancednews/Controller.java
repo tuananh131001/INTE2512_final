54034271f3604977ef5fa5062c624108412ad68e
RenuzitV <nvminhduy2002@gmail.com>
2021-09-14 00:59:25 +0700

    added headers

--- src/advancednews/Controller.java
+++ src/advancednews/Controller.java
@@ -1,3 +1,27 @@
+/*
+  RMIT University Vietnam
+  Course: INTE2512 Object-Oriented Programming
+  Semester: 2021B
+  Assessment: Final Project
+  Created  date: 09/06/2021
+  Author:
+  Nguyen Tuan Anh s3864077
+  Tran Nguyen Ha Khanh s3877707
+  Nguyen Vu Minh Duy s3878076
+  Phan Thanh Phu s3877814
+  Ngo Thanh Nguyen s3856221
+  Last modified date: 14/09/2021
+  Acknowledgement:
+  http://www.java2s.com/Tutorials/Java/JavaFX_How_to/Image/Load_an_Image_from_local_file_system.htm
+  https://docs.oracle.com/javafx/2/webview/jfxpub-webview.htm
+  https://stackoverflow.com/questions/6530974/getting-a-property-value-and-passing-it-on-to-superclass/6531076#6531076
+  https://stackoverflow.com/questions/47743650/javafx-8-property-bindings-for-custom-objects
+  https://stackoverflow.com/questions/21083945/how-to-avoid-not-on-fx-application-thread-currentthread-javafx-application-th
+  https://stackoverflow.com/questions/541487/implements-runnable-vs-extends-thread-in-java?page=2&tab=votes#tab-top
+  https://stackoverflow.com/questions/4691533/java-wait-for-thread-to-finish
+  https://stackoverflow.com/questions/13946372/adding-css-file-to-stylesheets-in-javafx
+*/
+
 package advancednews;
 
 import advancednews.Model.Article;
@@ -7,7 +31,6 @@ import javafx.animation.*;
 import javafx.beans.property.DoubleProperty;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -31,6 +54,8 @@ import javafx.util.Pair;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.net.CookieHandler;
+import java.net.CookieManager;
 import java.net.URL;
 import java.util.*;
 
@@ -80,6 +105,7 @@ public class Controller implements Initializable {
             //init web engine
             newsScene = new WebView();
             engine = newsScene.getEngine();
+            CookieHandler.setDefault(new CookieManager());
 
             //setting up news scrapers
             newsHashMap = new LinkedHashMap<>();
